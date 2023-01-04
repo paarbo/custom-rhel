5f428a96ecc881b1b33d0fd26fd8514e3577e0c2
paarbo <35691223+paarbo@users.noreply.github.com>
2023-01-04 22:36:44 +0000

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -2,7 +2,7 @@ dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86
 rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 dnf install -y https://download.onlyoffice.com/repo/centos/main/noarch/onlyoffice-repo.noarch.rpm
 dnf install -y epel-release
-dnf install -y gdm gnome-shell gnome-terminal brave-browser nautilus onlyoffice-desktopeditors gnome-tweaks gnome-extensions-app gnome-browser-connector gnome-usage gnome-password-generator gnome-disk-utility gnome-photos totem gnome-calculator gnome-documents gnome-weather gnome-text-editor gtk-murrine-engine
+dnf install -y gdm gnome-shell gnome-terminal brave-browser nautilus onlyoffice-desktopeditors gnome-tweaks gnome-extensions-app gnome-browser-connector gnome-usage gnome-password-generator gnome-disk-utility gnome-photos totem gnome-calculator gnome-documents gnome-weather gnome-text-editor gtk-murrine-engine sassc
 systemctl enable gdm
 systemctl set-default graphical.target
 dnf remove/purge gnome-tour
