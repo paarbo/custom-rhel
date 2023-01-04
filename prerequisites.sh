d30f2ff28dc58749a53a489bf6358966a3e415ce
paarbo <35691223+paarbo@users.noreply.github.com>
2023-01-04 22:38:39 +0000

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,7 +1,7 @@
 dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 dnf install -y https://download.onlyoffice.com/repo/centos/main/noarch/onlyoffice-repo.noarch.rpm
-dnf install -y epel-release
+dnf config-manager --add-repo https://rpm.librewolf.net/librewolf-repo.repo
 dnf install -y gdm gnome-shell gnome-terminal brave-browser nautilus onlyoffice-desktopeditors gnome-tweaks gnome-extensions-app gnome-browser-connector gnome-usage gnome-password-generator gnome-disk-utility gnome-photos totem gnome-calculator gnome-documents gnome-weather gnome-text-editor gtk-murrine-engine sassc
 systemctl enable gdm
 systemctl set-default graphical.target
