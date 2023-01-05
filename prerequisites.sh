6f78b65cf762e01e131c415f07e0cc0de7d99c98
paarbo <35691223+paarbo@users.noreply.github.com>
2023-01-05 18:06:24 +0000

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -3,7 +3,36 @@ dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86
 rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 dnf install -y https://download.onlyoffice.com/repo/centos/main/noarch/onlyoffice-repo.noarch.rpm
 dnf config-manager --add-repo https://rpm.librewolf.net/librewolf-repo.repo
-dnf install -y gdm gnome-shell gnome-terminal brave-browser nautilus onlyoffice-desktopeditors gnome-tweaks gnome-extensions-app gnome-browser-connector gnome-usage gnome-password-generator gnome-disk-utility gnome-photos totem gnome-calculator gnome-documents gnome-weather gnome-text-editor gtk-murrine-engine sassc
+dnf install -y gdm gnome-shell gnome-terminal brave-browser nautilus onlyoffice-desktopeditors gnome-tweaks gnome-extensions-app gnome-browser-connector gnome-usage gnome-password-generator gnome-disk-utility gnome-photos totem gnome-calculator gnome-documents gnome-weather gnome-text-editor gtk-murrine-engine sassc ufw fail2ban
 systemctl enable gdm
 systemctl set-default graphical.target
 dnf remove/purge gnome-tour
+# --- Setup UFW rules
+ufw allow 80/tcp  
+ufw allow 443/tcp  
+ufw default deny incoming  
+ufw default allow outgoing
+ufw enable
+
+# --- Harden /etc/sysctl.conf
+sysctl kernel.modules_disabled=1
+sysctl -a
+sysctl -A
+sysctl mib
+sysctl net.ipv4.conf.all.rp_filter
+sysctl -a --pattern 'net.ipv4.conf.(eth|wlan)0.arp'
+
+# --- PREVENT IP SPOOFS
+cat <<EOF > /etc/host.conf
+order bind,hosts
+multi on
+EOF
+
+# --- Enable fail2ban
+wget https://raw.githubusercontent.com/ChrisTitusTech/secure-linux/master/jail.local
+cp jail.local /etc/fail2ban/
+systemctl enable fail2ban
+systemctl start fail2ban
+
+echo "listening ports"
+netstat -tunlp
