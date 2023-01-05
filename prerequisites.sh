173ab889658b16c98bd98476a2244dcdc81f6c28
paarbo <35691223+paarbo@users.noreply.github.com>
2023-01-05 14:33:45 +0000

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,3 +1,4 @@
+dnf update -y
 dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 dnf install -y https://download.onlyoffice.com/repo/centos/main/noarch/onlyoffice-repo.noarch.rpm
