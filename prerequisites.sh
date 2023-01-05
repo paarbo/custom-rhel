2bf44d9621c216ae8ebc62ec027907df71eec3d4
paarbo <35691223+paarbo@users.noreply.github.com>
2023-01-05 19:16:39 +0000

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -3,7 +3,7 @@ dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86
 rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 dnf install -y https://download.onlyoffice.com/repo/centos/main/noarch/onlyoffice-repo.noarch.rpm
 dnf config-manager --add-repo https://rpm.librewolf.net/librewolf-repo.repo
-dnf install -y gdm gnome-shell gnome-terminal brave-browser nautilus onlyoffice-desktopeditors gnome-tweaks gnome-extensions-app gnome-browser-connector gnome-usage gnome-password-generator gnome-disk-utility gnome-photos totem gnome-calculator gnome-weather gnome-text-editor gtk-murrine-engine sassc ufw fail2ban
+dnf install -y gdm gnome-shell gnome-terminal brave-browser nautilus onlyoffice-desktopeditors gnome-tweaks gnome-extensions-app gnome-browser-connector gnome-usage gnome-password-generator gnome-disk-utility gnome-photos totem gnome-calculator gnome-weather gnome-text-editor gtk-murrine-engine sassc ufw fail2ban net-tools
 #gnome-documents not found
 systemctl enable gdm
 systemctl set-default graphical.target
@@ -17,9 +17,9 @@ ufw default allow outgoing
 ufw enable
 
 # --- Harden /etc/sysctl.conf
-sysctl kernel.modules_disabled=1
-sysctl -a
-sysctl -A
+#sysctl kernel.modules_disabled=1
+#sysctl -a
+#sysctl -A
 #sysctl mib
 sysctl net.ipv4.conf.all.rp_filter
 sysctl -a --pattern 'net.ipv4.conf.(eth|wlan)0.arp'
@@ -36,5 +36,5 @@ cp fail2ban.local /etc/fail2ban/
 systemctl enable fail2ban
 systemctl start fail2ban
 
-#echo "listening ports"
-#netstat -tunlp
+echo "listening ports"
+netstat -tunlp
