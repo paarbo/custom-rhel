54851c99790fd584fd4de087a9f5e068a5477327
paarbo <35691223+paarbo@users.noreply.github.com>
2023-01-24 20:57:00 +0000

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,9 +1,7 @@
-#requires root perms
-dnf update -y
-dnf autoremove -y
-flatpak upgrade -y
-#goes to the desired user, to avoid any file owner issues
-su - $1
+#requires sudo perms
+sudo dnf update -y
+sudo dnf autoremove -y
+sudo flatpak upgrade -y
 git clone https://github.com/vinceliuice/Fluent-gtk-theme.git
 git clone https://github.com/vinceliuice/Fluent-icon-theme.git
 sh ./Fluent-gtk-theme/install.sh -l --tweaks round noborder square
