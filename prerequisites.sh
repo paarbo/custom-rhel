3d5e8dfa7d0863bfa04eaef39f0837e33989fc28
paarbo <35691223+paarbo@users.noreply.github.com>
2023-05-13 12:17:28 +0000

    Update prerequisites.sh

--- prerequisites.sh
+++ prerequisites.sh
@@ -1,4 +1,9 @@
 dnf update -y
+dnf install -y unzip gdm gnome-shell gnome-terminal gnome-system-monitor nautilus gnome-software gnome-tweaks gnome-extensions-app chrome-gnome-shell gnome-disk-utility gnome-photos gnome-menus totem gnome-calculator gedit seahorse git
+systemctl enable gdm
+systemctl set-default graphical.target
+dnf remove -y gnome-tour
+
 dnf config-manager --set-enabled crb
 dnf install -y epel-release
 dnf install -y rpmfusion-free-release
@@ -8,17 +13,14 @@ rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 dnf install https://download.onlyoffice.com/repo/centos/main/noarch/onlyoffice-repo.noarch.rpm -y
 dnf config-manager --add-repo https://rpm.librewolf.net/librewolf-repo.repo
 dnf groupupdate core -y
-dnf update -y
-dnf install -y unzip gdm gnome-shell gnome-terminal okular gnome-software brave-browser nautilus librewolf onlyoffice-desktopeditors gnome-tweaks gnome-extensions-app chrome-gnome-shell gnome-disk-utility gnome-photos totem gnome-calculator gedit gtk-murrine-engine gnome-menus sassc ufw fail2ban file-roller seahorse git
+dnf update
+dnf install -y okular brave-browser librewolf onlyoffice-desktopeditors gtk-murrine-engine sassc ufw fail2ban file-roller
 # net-tools not found gnome-console gnome-usage gnome-password-generator gnome-text-editor gnome-weather gnome-browser-connector
 # https://apps.gnome.org news-flash dialect obfuscate warp metadata-cleaner
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak update -y
-flatpak install flathub -y app.drey.Warp app.drey.Dialect com.gitlab.newsflash com.belmoussaoui.Obfuscate fr.romainvigier.MetadataCleaner
+flatpak install flathub -y app.drey.Warp app.drey.Dialect com.gitlab.newsflash com.belmoussaoui.Obfuscate fr.romainvigier.MetadataCleaner #com.moonlight_stream.Moonlight
 
-systemctl enable gdm
-systemctl set-default graphical.target
-dnf remove -y gnome-tour
 dnf autoremove -y
 # --- Setup UFW rules
 #ufw limit 22/tcp
@@ -45,6 +47,7 @@ EOF
 # --- Enable fail2ban
 wget https://raw.githubusercontent.com/ChrisTitusTech/secure-linux/master/jail.local
 cp jail.local /etc/fail2ban/jail.local
+rm -f jail.local
 systemctl enable fail2ban
 systemctl start fail2ban
 
